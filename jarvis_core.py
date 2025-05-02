cccb709f11fef3d72f724a57d0fb2dfaa53aef4a
Brunotardiiiiollli <brunotardioli4@gmail.com>
2025-05-02 06:49:59 -0300

    Update jarvis_core.py

--- jarvis_core.py
+++ jarvis_core.py
@@ -3,7 +3,7 @@ import pyttsx3
 import speech_recognition as sr
 
 # Chave da OpenAI embutida diretamente no código
-openai.api_key = "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"
+openai.api_key = "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"
 
 engine = pyttsx3.init()
 recognizer = sr.Recognizer()
